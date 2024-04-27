c7f6de0d7ef581333bfad7e5416310b106943ca6
Renan1404 <82480715+Renan1404@users.noreply.github.com>
2024-04-27 00:47:37 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -12,11 +12,20 @@ app.use(express.json());
 
 
 mongoose
-.connect('mongodb://127.0.0.1:27017/authentication')
-.then(() => console.log('MongoDB Connected...'))
-.catch((error) => console.log('Failed to connect to MongoDB:', error));
+  .connect('mongodb://127.0.0.1:27017/authentication')
+  .then(() => console.log('MongoDB Connected...'))
+  .catch((error) => console.log('Failed to connect to MongoDB:', error));
 
 
+app.use((err, res, req, next) => {
+    err.statuCode = err.statuCode || 500;
+    err.status = err.status || "error";
+
+    res.status(err.statuCode).json({
+        status : err.status ,
+        message : err.message,
+    });
+});
 
 
 const PORT = 3000;
